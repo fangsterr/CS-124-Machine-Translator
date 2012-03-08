aee007266c3f506384cb773551fd8d400ef92b9c
Andy Fang <andyfang@stanford.edu>
2012-03-07 16:42:30 -0800

    Implemented translation method

--- trunk/DictionaryUtil.java
+++ trunk/DictionaryUtil.java
@@ -12,6 +12,9 @@ class DictionaryUtil {
 	 * Imports from a file and returns a Map mapping foreign word Strings to
 	 * English word Strings. Each line in the file should be of the format
 	 * "foreign|english".
+	 * 
+	 * Uncapitalizes foreign word before placing it in dictionary, although the
+	 * English casing is preserved.
 	 */
 	public static Map<String, String> importDictionary(String filename) {
 		HashMap<String, String> map = new HashMap<String, String>();
@@ -19,13 +22,13 @@ class DictionaryUtil {
 			BufferedReader in = new BufferedReader(new FileReader(filename));
 			String line;
 			while ((line = in.readLine()) != null) {
-				if (line.isEmpty() || line.charAt(0) == '#') continue;
+				if (line.isEmpty() || line.charAt(0) == '#')
+					continue;
 				StringTokenizer st = new StringTokenizer(line, "|");
-				String foreign = st.nextToken().trim();
+				String foreign = st.nextToken().trim().toLowerCase();
 				String english = st.nextToken().trim();
 
 				map.put(foreign, english);
-				System.out.println(foreign + ", " + english);
 			}
 			return map;
 		} catch (Exception e) {
@@ -37,15 +40,42 @@ class DictionaryUtil {
 	/**
 	 * 
 	 */
-	public static void translateText(Map<String,String> dict, String inFile,
-									 String outFile) {
+	public static void translateText(Map<String, String> dict, String inFile,
+			String outFile) {
 		try {
-			BufferedReader in  = new BufferedReader(new FileReader(inFile));
+			BufferedReader in = new BufferedReader(new FileReader(inFile));
+				// foreign text
 			BufferedWriter out = new BufferedWriter(new FileWriter(outFile));
-			// out.write()
+				// english output
+			
+			String fSentence;
+			while ((fSentence = in.readLine()) != null) {
+				String eSentence = "";
+				StringTokenizer st = new StringTokenizer(fSentence);
+				while (st.hasMoreTokens()) {
+					String fWord = st.nextToken().toLowerCase();
+					boolean eos = false; // end of sentence
+					if (fWord.charAt(fWord.length()-1) == '.') {
+						// get rid of end of sentence periods
+						fWord = fWord.substring(0, fWord.length()-1);
+						eos = true;
+					}
+					String eWord = dict.get(fWord);
+					if (eWord == null) eWord = fWord;
+					eSentence += eWord + (eos ? "." : "") + " ";
+				}
+				out.write(eSentence.trim());
+			}
 			out.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	public static void main(String[] args) {
+		Map<String, String> dict = DictionaryUtil
+				.importDictionary("dictionary.txt");
+		
+		
 	}
 }
