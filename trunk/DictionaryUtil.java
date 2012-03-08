9cdb5b1e76d48db34284ae0b45a904d9907a4db0
Andy Fang <andyfang@stanford.edu>
2012-03-07 17:25:11 -0800

    Fixed punctuation parsing issues

--- trunk/DictionaryUtil.java
+++ trunk/DictionaryUtil.java
@@ -15,6 +15,9 @@ class DictionaryUtil {
 	 * 
 	 * Uncapitalizes foreign word before placing it in dictionary, although the
 	 * English casing is preserved.
+	 * 
+	 * @param filename
+	 * @return dictionary mapping foreign to english
 	 */
 	public static Map<String, String> importDictionary(String filename) {
 		HashMap<String, String> map = new HashMap<String, String>();
@@ -25,7 +28,7 @@ class DictionaryUtil {
 				if (line.isEmpty() || line.charAt(0) == '#')
 					continue;
 				StringTokenizer st = new StringTokenizer(line, "|");
-				String foreign = st.nextToken().trim().toLowerCase();
+				String foreign = st.nextToken().trim();
 				String english = st.nextToken().trim();
 
 				map.put(foreign, english);
@@ -38,34 +41,95 @@ class DictionaryUtil {
 	}
 
 	/**
+	 * Converts a String to title-case
+	 */
+	private static String toTitleCase(String s) {
+		return Character.toUpperCase(s.charAt(0)) + s.substring(1);
+	}
+	
+	private static String getPrePunctuation(String s) {
+		for (int i = 0; i < s.length(); i++) {
+			if (Character.isLetter(s.charAt(i))) {
+				return s.substring(0, i);
+			}
+		}
+		
+		return "";
+	}
+	
+	private static String getPostPunctuation(String s) {
+		for (int i = s.length()-1; i >= 0; i--) {
+			if (Character.isLetter(s.charAt(i))) {
+				if (i == s.length()-1) return "";
+				return s.substring(i+1);
+			}
+		}
+		return "";
+	}
+
+	/**
+	 * Filters non-letter from a non-numeric word
+	 */
+	private static String filterWord(String s) {
+		String out = "";
+
+		for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+			if (Character.isDigit(c)) return s; // don't filter numbers
+			if (Character.isLetter(c)) {
+				out += c;
+			}
+		}
+
+		return out;
+	}
+
+	/**
 	 * 
 	 */
 	public static void translateText(Map<String, String> dict, String inFile,
 			String outFile) {
 		try {
 			BufferedReader in = new BufferedReader(new FileReader(inFile));
-				// foreign text
+			// foreign text
 			BufferedWriter out = new BufferedWriter(new FileWriter(outFile));
-				// english output
-			
+			// english output
+
 			String fSentence;
 			while ((fSentence = in.readLine()) != null) {
-				if (fSentence.isEmpty()) continue;
+				if (fSentence.isEmpty())
+					continue;
 				
 				String eSentence = "";
 				StringTokenizer st = new StringTokenizer(fSentence);
 				while (st.hasMoreTokens()) {
-					String fWord = st.nextToken().toLowerCase();
-					boolean eos = false; // end of sentence
-					if (fWord.charAt(fWord.length()-1) == '.') {
-						// get rid of end of sentence periods
-						fWord = fWord.substring(0, fWord.length()-1);
-						eos = true;
+					String token = st.nextToken();
+					String fWord = filterWord(token);
+					String prefix, suffix;
+					prefix = suffix = "";
+					if (fWord.length() != token.length()) {
+						// if filtering occurred
+						prefix = getPrePunctuation(token);
+						suffix = getPostPunctuation(token);
+						System.out.println(prefix + "||" + suffix);
+						if (fWord.isEmpty()) continue;
 					}
+
 					String eWord = dict.get(fWord);
-					if (eWord == null) eWord = fWord;
-					eSentence += eWord + (eos ? "." : "") + " ";
+					if (eWord == null) {
+						if (dict.get(fWord.toLowerCase()) != null) {
+							// if foreign word is capitalized for some reason
+							eWord = toTitleCase(dict.get(fWord.toLowerCase()));
+						} else {
+							// if foreign word not found in dictionary
+							eWord = fWord;
+						}
+					}
+					eSentence += prefix + eWord + suffix + " ";
 				}
+				eSentence = eSentence.trim();
+				eSentence = Character.toUpperCase(eSentence.charAt(0))
+						+ eSentence.substring(1);
 				out.write(eSentence.trim());
 				out.newLine();
 			}
