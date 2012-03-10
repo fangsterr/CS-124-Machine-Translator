f63d825b69ec41da6b66b6da8274e2ebf7bfbaf1
Andy Fang <andyfang@stanford.edu>
2012-03-10 11:32:42 -0800

    Added digits rule, fixed typo in dictionary

--- trunk/MT.java
+++ trunk/MT.java
@@ -7,7 +7,7 @@ import java.util.*;
  * This class is a utility to import a dictionary mapping a foreign word to an
  * English word.
  */
-class DictionaryUtil {
+class MT {
 	/** CONSTANTS **/
 	private static final char TAGGED_DELIM = '_';
 
@@ -15,9 +15,6 @@ class DictionaryUtil {
 	 * Imports from a file and returns a Map mapping foreign word Strings to
 	 * English word Strings. Each line in the file should be of the format
 	 * "foreign|english".
-	 * 
-	 * Uncapitalizes foreign word before placing it in dictionary, although the
-	 * English casing is preserved.
 	 * 
 	 * @param filename
 	 * @return dictionary mapping foreign to english
@@ -101,8 +98,8 @@ class DictionaryUtil {
 				// if foreign word is capitalized for some reason
 				eWord = toTitleCase(dict.get(fWord.toLowerCase()));
 			} else if (fWord.indexOf('-') != -1) {
-				// check to see if the word is repeated e.g. "obat-obat"
-				// repeated words means plural
+				/** Rule: check to see if the word is repeated e.g. "obat-obat"
+				  		 (repeated words means plural) **/
 				int hyphen = fWord.indexOf('-');
 				if (fWord.substring(0, hyphen).equals(
 						fWord.substring(hyphen + 1))) {
@@ -196,26 +193,77 @@ class DictionaryUtil {
 	 * outputs a rewritten English sentence.
 	 */
 	public static String rewriteSentence(ArrayList<String> tagged) {
-		// Rule: Switch NOUN-ADJ to ADJ-NOUN
-		// (also sets up for Rule 2
+		/** Rule: Switch NOUN-ADJ to ADJ-NOUN
+		   (also sets up for article rule) **/
 		for (int i = 0; i < tagged.size() - 1; i++) {
 			String word1 = tagged.get(i);
 			String word2 = tagged.get(i + 1);
 			if (isNoun(word1) && isAdjective(word2)) {
-				// System.out.println(word2 + " " + word1);
 				tagged.set(i, word2 + "THE");
 				tagged.set(i + 1, word1);
 			}
 
 		}
 
-		// Rule: Adjust time phrases
+		/** Rule: Adjust time phrases (year 1992 -> 1992) **/
+		for (int i = 0; i < tagged.size()-1; i++) {
+			String rawWord1 = tagged.get(i);
+			String rawWord2 = tagged.get(i+1);
+			String word1 = rawWord1.substring(0, rawWord1.indexOf(TAGGED_DELIM));
+			String word2 = rawWord2.substring(0, rawWord2.indexOf(TAGGED_DELIM));
 
-		// Rule:
+			if (word1.toLowerCase().equals("year") && word2.length() == 4) {
+				if (isNumberNotTagged(word2)) {
+					tagged.set(i, "REMOVE");
+				}
+			} else if (word1.toLowerCase().equals("month") && word2.length() < 3) {
+				if (isNumberNotTagged(word2)) {
+					int month = Integer.parseInt(word2);
+					if (month >= 1 && month <= 12) {
+						tagged.set(i, "REMOVE");
+					}
+				}
+			} else if (word1.toLowerCase().equals("day") && word2.length() < 3) {
+				if (isNumberNotTagged(word2)) {
+					int day = Integer.parseInt(word2);
+					if (day >= 1 && day <= 31) {
+						tagged.set(i, "REMOVE");
+					}
+				}
+			}
+		}
+		while (tagged.remove("REMOVE"));
 
-		// Rule: Switch NOT-modal to modal-NOT (not will -> will not)
+		/** Rule: Fix numbers to display correct English format (1,9 -> 1.9) **/
+		for (int i = 0; i < tagged.size(); i++) {
+			String word = tagged.get(i);
+			if (isNumber(word)) {
+				String newWord = "";
+				for (int j = 0; j < word.length(); j++) {
+					if (word.charAt(j) == ',') {
+						newWord += ".";
+					} else if (word.charAt(j) == '.') {
+						newWord += ",";
+					} else {
+						newWord += word.charAt(j);
+					}
+				}
+				tagged.set(i, newWord);
+			}
+		}
 
-		// Rule: Break up consecutive nouns into pairs and add 'of' after
+		/** Rule: Switch NOT-modal to modal-NOT (not will -> will not) **/
+		for (int i = 0; i < tagged.size() - 1; i++) {
+			String word1 = tagged.get(i);
+			String word2 = tagged.get(i+1);
+			if (isModal(word2) && word1.toLowerCase().startsWith("not")) {
+				tagged.set(i, word2);
+				tagged.set(i+1, word1);
+			}
+		}
+		
+
+		/** Rule: Break up consecutive nouns into pairs and add 'of' after **/
 		int count = 0;
 		for (int i = 0; i < tagged.size(); i++) {
 			String word = tagged.get(i);
@@ -231,7 +279,7 @@ class DictionaryUtil {
 			}
 		}
 
-		// Rule: Switch NOUN-NOUN phrases (team badminton -> badminton team)
+		/** Rule: Switch NOUN-NOUN phrases (team baseball -> baseball team) **/
 		for (int i = 0; i < tagged.size() - 1; i++) {
 			String word1 = tagged.get(i);
 			String word2 = tagged.get(i + 1);
@@ -248,7 +296,7 @@ class DictionaryUtil {
 			}
 		}
 
-		// Rule: Add articles to all noun phrases
+		/** Rule: Add articles to all noun phrases **/
 		if (isNoun(tagged.get(0))) {
 			tagged.set(0, tagged.get(0) + "THE");
 		}
@@ -261,7 +309,7 @@ class DictionaryUtil {
 			}
 		}
 
-		/** Process into actual sentence **/
+		// Process into actual sentence
 		String out = "";
 		for (int i = 0; i < tagged.size(); i++) {
 			String word = tagged.get(i);
@@ -300,13 +348,40 @@ class DictionaryUtil {
 		return (taggedWord.charAt(index + 1) == 'J')
 				|| (taggedData.startsWith("DT"));
 	}
+	
+	private static boolean isModal(String taggedWord) {
+		int index = taggedWord.indexOf(TAGGED_DELIM);
+		if (index == -1)
+			return false;
+		String taggedData = taggedWord.substring(index + 1);
+		return taggedData.startsWith("MD");
+	}
+	
+	private static boolean isNumber(String taggedWord) {
+		int index = taggedWord.indexOf(TAGGED_DELIM);
+		if (index == -1)
+			return false;
+		String taggedData = taggedWord.substring(index + 1);
+		return taggedData.startsWith("CD");
+	}
+	
+	private static boolean isNumberNotTagged(String word) {
+		boolean wordIsNum = true;
+		for (int j = 0; j < word.length(); j++) {
+			if (!Character.isDigit(word.charAt(j))) {
+				wordIsNum = false;
+				break;
+			}
+		}
+		return wordIsNum;
+	}
 
 	public static void main(String[] args) {
-		Map<String, String> dict = DictionaryUtil
+		Map<String, String> dict = MT
 				.importDictionary("dictionary.txt");
-		DictionaryUtil.translateRawText(dict, "indo_input.txt",
+		MT.translateRawText(dict, "indo_input.txt",
 				"english_output.txt");
-		DictionaryUtil.updateTaggedText("english_tagged.txt",
+		MT.updateTaggedText("english_tagged.txt",
 				"final_english.txt");
 	}
 }
